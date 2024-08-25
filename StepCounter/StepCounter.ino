4dc9edd908f2b969a29beba70d77c62e52040953
TEQ-p <takun.purba@gmail.com>
2024-08-25 10:54:32 -0700

    Stripped to only the neccessary dependancies

--- StepCounter/StepCounter.ino
+++ StepCounter/StepCounter.ino
@@ -1,71 +1,54 @@
 #include <Arduino.h>
-#include "LCD_Test.h"
-UWORD Imagesize = LCD_1IN28_HEIGHT * LCD_1IN28_WIDTH * 2;// set the screen size 
-UWORD *BlackImage;//declares a pointer
-CST816S touch(6, 7, 13, 5);	// sda, scl, rst, irq <- these are the corresponding pins
+// #include "LCD_Test.h"
+#include "DEV_Config.h"
+#include "QMI8658.h"
+#include <Wire.h>
 
-// const float conversion_factor = 3.3f / (1 << 12) * 3;
+#define QMI8658_ADDR 0x6B  // I2C address of the QMI8658 sensor
+
+// Register Addresses (example addresses, adjust based on your setup)
+#define CTRL7 0x1C
+#define CTRL8 0x1D
+#define CTRL9 0x1E
+#define STATUS1 0x1F
+#define STEP_CNT_LOW 0x30
+#define STEP_CNT_MIDL 0x31
+#define STEP_CNT_HIGH 0x32
+#define CAL1_L 0x21
+#define CAL1_H 0x22
+#define CAL2_L 0x23
+#define CAL2_H 0x24
+#define CAL3_L 0x25
+#define CAL3_H 0x26
+#define CAL4_H 0x28
+
+void writeRegister(uint8_t reg, uint8_t value) {
+  Wire.beginTransmission(QMI8658_ADDR);
+  Wire.write(reg);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+uint8_t readRegister(uint8_t reg) {
+  Wire.beginTransmission(QMI8658_ADDR);
+  Wire.write(reg);
+  Wire.endTransmission();
+  Wire.requestFrom(QMI8658_ADDR, 1);
+  return Wire.read();
+}
 
 void setup() {
   Serial.begin(115200);
   
-  touch.begin();//initializes touch input 
-  
-  // PSRAM Initialize-> pseudo static random access Memory 
-  if(psramInit()){
-    Serial.println("\nPSRAM is correctly initialized");
-  }else{
-    Serial.println("PSRAM not available");
-  }
-  
-  if ((BlackImage = (UWORD *)ps_malloc(Imagesize)) == NULL){//Memory Allocation for black image 
-      Serial.println("Failed to apply for black memory...");
-      exit(0);
-  }
-  // put your setup code here, to run once:
   if (DEV_Module_Init() != 0)//initializes GPIO pins 
     Serial.println("GPIO Init Fail!");
   else
     Serial.println("GPIO Init successful!");
-    // LCD_1IN28_Init(HORIZONTAL);//initializes lcd 
-    // LCD_1IN28_Clear(WHITE);  //clears the lcd screen and fills it with white color 
-    // // /*1.Create a new image cache named IMAGE_RGB and fill it with white*/
-    // Paint_NewImage((UBYTE *)BlackImage, LCD_1IN28.WIDTH, LCD_1IN28.HEIGHT, 0, WHITE);//create a buffer and fill it with white color 
-    // Paint_SetScale(65);//set the drawing scale factor to 65% of the original 
-    // Paint_SetRotate(ROTATE_0);//sets the rotation angle of the drawing 0 degrees 
-    // // Paint_Clear(WHITE);//clear the drawing context sets it to white
-    // /* GUI */
-    // Serial.println("drawing...\r\n");
-    // delay(2000);//delay 2 seconds 
-    // LCD_1IN28_Clear(RED);
-    // delay(2000);
-    // LCD_1IN28_Clear(GREEN);
-    // delay(2000);
-    // LCD_1IN28_Clear(BLUE);
-    // delay(2000);
 #if 1
   float acc[3], gyro[3];
   unsigned int tim_count = 0;
   uint16_t result;
-
   QMI8658_init();
-
-  // Paint_Clear(WHITE);
-  // Paint_DrawRectangle(0, 00, 240, 47, 0XF410, DOT_PIXEL_2X2, DRAW_FILL_FULL);
-  // Paint_DrawRectangle(0, 47, 240, 120, 0X4F30, DOT_PIXEL_2X2, DRAW_FILL_FULL);
-  // Paint_DrawRectangle(0, 120, 240, 195, 0XAD55, DOT_PIXEL_2X2, DRAW_FILL_FULL);
-  // Paint_DrawRectangle(0, 195, 240, 240, 0X2595, DOT_PIXEL_2X2, DRAW_FILL_FULL);
-  // //todo get it to print out the values its equal to
-  // Paint_DrawString_EN(45, 30, "LongPress Quit", &Font16, WHITE, BLACK);
-  // Paint_DrawString_EN(45, 50, "ACC_X = ", &Font16, WHITE, BLACK);
-  // Paint_DrawString_EN(45, 75, "ACC_Y = ", &Font16, WHITE, BLACK);
-  // Paint_DrawString_EN(45, 100, "ACC_Z = ", &Font16, WHITE, BLACK);
-  // Paint_DrawString_EN(45, 125, "GYR_X = ", &Font16, WHITE, BLACK);
-  // Paint_DrawString_EN(45, 150, "GYR_Y = ", &Font16, WHITE, BLACK);
-  // Paint_DrawString_EN(45, 175, "GYR_Z = ", &Font16, WHITE, BLACK);
-  // // Paint_DrawString_EN(45, 200, "BAT(V)=", &Font16, WHITE, BLACK);
-  // LCD_1IN28_Display(BlackImage);
-
   while (true)
   {
     result = DEC_ADC_Read();
@@ -82,28 +65,12 @@ void setup() {
     Serial.println(gyro[1]);
     Serial.println("GYR_Z = " );
     Serial.println(gyro[2]);
-    
-    // Paint_Clear(WHITE);
-    // Paint_DrawRectangle(120, 47,  220, 120, 0X4F30, DOT_PIXEL_2X2, DRAW_FILL_FULL);
-    // Paint_DrawRectangle(120, 120, 220, 195, 0XAD55, DOT_PIXEL_2X2, DRAW_FILL_FULL);
-    // Paint_DrawRectangle(120, 195, 220, 240, 0X2595, DOT_PIXEL_2X2, DRAW_FILL_FULL);
-    // Paint_DrawNum(120, 50, acc[0], &Font16, 2, BLACK, WHITE);
-    // Paint_DrawNum(120, 75, acc[1], &Font16, 2, BLACK, WHITE);
-    // Paint_DrawNum(120, 100, acc[2], &Font16, 2, BLACK, WHITE);
-    // Paint_DrawNum(120, 125, gyro[0], &Font16, 2, BLACK, WHITE);
-    // Paint_DrawNum(120, 150, gyro[1], &Font16, 2, BLACK, WHITE);
-    // Paint_DrawNum(120, 175, gyro[2], &Font16, 2, BLACK, WHITE);
-    // // Paint_DrawNum(130, 200, result * conversion_factor, &Font16, 2, BLACK, WHITE);
-    // LCD_1IN28_DisplayWindows(120, 50, 210, 200, BlackImage);
-    // LCD_1IN28_DisplayWindows(130, 200, 220, 220, BlackImage);
-    // if (touch.available()){
-    //   if(touch.data.y<45){
-    //     break;
-    //   }
-    // }
   }
 #endif
 }
 
 void loop() {
+}
+void init_Pedometer(){
+
 }
